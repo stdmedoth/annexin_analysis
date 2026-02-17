d5b2770b676d809cee4357f7ce60653f241714bc
stdmedoth <jovictor210@gmail.com>
2026-02-17 13:58:40 -0300

    updates on load pdb

--- main.py
+++ main.py
@@ -1,41 +1,31 @@
 import glob
 import mdtraj as md
 import warnings
-
+import numpy as np
 
 import matplotlib.pyplot as plt
 
 # for bioemu
 warnings.filterwarnings("ignore", message="Unlikely unit cell vectors detected")
 
-full_ref = md.load('pdbs/AF-P50995-F1-model_v6.pdb')
 
-ca_indices = full_ref.topology.select('name CA')
-ref_ca = full_ref.atom_slice(ca_indices)
+traj = md.load('pdbs/samples.xtc', top='pdbs/topology.pdb')
+ca_indices = traj.topology.select('name CA')
+traj_ca = traj.atom_slice(ca_indices)
+traj_ca.superpose(traj_ca, 0)
+fluctuations = md.rmsf(traj_ca, traj_ca, 0)
 
-files = sorted(glob.glob('pdbs/batch_*.pdb'))
-target_traj = md.load(files)
+print(fluctuations.mean())
 
-target_traj.superpose(ref_ca, frame=0)
-
-#core_indices = target_traj.topology.select("resid 197 to 505")
-core_indices = target_traj.topology.select("resid 199 to 504")
-
-core_traj = target_traj.atom_slice(core_indices)
-ref_core = ref_ca.atom_slice(core_indices)
-
-core_traj.superpose(ref_core)
-
-rmsf = md.rmsf(core_traj, ref_core)
-#rmsf = md.rmsf(core_traj, ref_traj, atom_indices=core_indices)
-#rmsd = md.rmsd(target_traj, ref_ca, atom_indices=core_indices)
 
 plt.figure(figsize=(10,5))
-#plt.plot(rmsf, color="#2c3e50", linewidth=1.5)
-residue_numbers = range(199, 199 + len(rmsf))
-plt.plot(residue_numbers, rmsf * 10, color="#2c3e50", linewidth=1.5)
-#plt.axvspan(0,199, color='gray', alpha=0.2, label='N-Terminal')
-#plt.axvspan(199,505, color='blue', alpha=0.1, label='Annexin core')
+#plt.plot(fluctuations, color="#2c3e50", linewidth=1.5)
+
+residue_numbers = range(0, len(fluctuations))
+plt.plot(residue_numbers, fluctuations, color="#2c3e50", linewidth=1.5)
+plt.axvspan(0,199, color='gray', alpha=0.2, label='N-Terminal')
+plt.axvspan(199,505, color='blue', alpha=0.1, label='Annexin core')
+plt.legend()
 
 
 plt.title('Conformational Profile: RMSF by Residue - Annexin A11', fontsize=14)
@@ -46,9 +36,3 @@ plt.grid(True, linestyle='--', alpha=0.6)
 plt.savefig('conformational_profile.png', dpi=300)
 plt.show()
 
-#mean = rmsd.mean()
-
-#print(rmsf, rmsd)
-
-#print(rmsf)
-#print(rmsd, mean)
